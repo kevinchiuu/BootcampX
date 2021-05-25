91ec4e95bd51065123c4070084604bffa221ac68
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-05-25 03:30:15 -0400

    add average completion time query

---
+++ 3_queries_group-by/average_completion_time.sql
@@ -0,0 +1,6 @@
+SELECT students.name AS student, avg(assignment_submissions.duration*) AS avg_submission_time
+FROM assignment_submissions
+JOIN assignment_submissions ON students_id = student_id
+WHERE end_date IS NULL
+GROUP BY student 
+ORDER BY avg_submission_time DESC
