77d31ec085a5ff3b5bc0546ebe878d2010858024
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-05-25 03:03:49 -0400

    fix typo

--- 3_queries_group-by/large_cohorts.sql
+++ 3_queries_group-by/large_cohorts.sql
@@ -2,5 +2,5 @@ SELECT cohorts.name as cohort_name, count(students*) AS student_count
 FROM cohorts
 JOIN students ON cohorts.id = cohort.id
 GROUP BY cohort_name
-HAVING count(*) >= 18
+HAVING count(students*) >= 18
 ORDER BY student_count;
