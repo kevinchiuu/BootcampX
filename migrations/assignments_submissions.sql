7e756401c216aa146726e86bd019738e8c007f9d
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-05-25 21:47:20 -0400

    add tables to database

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -4,13 +4,13 @@ CREATE TABLE assignments (
   content TEXT,
   day INTEGER,
   chapter INTEGER,
-  duration INTEGER
+  duration INTEGER,  
 );
 
 CREATE TABLE assignment_submissions (
   id SERIAL PRIMARY KEY NOT NULL, 
-  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignment(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   duration INTEGER,
   submission_date DATE
-)ee
+);
